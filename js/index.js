1b700b8e054861c4acd20f90892df1a07a0c4a20
Maxislav <lmaxim@mail.ru>
2014-07-06 23:59:25 +0300

    Обновление всего!

--- js/index.js
+++ js/index.js
@@ -1,9 +1,15 @@
 $(document).ready(function(){
+
+    var marginTop;
+    if($(window).width()<980){
+        marginTop = 210
+    }else{
+        marginTop = 180
+    }
 	$('.post__article').scrollNav({
 		headlineText: '',
 		topLinkText: 'Top',
-		//fixedMargin: 140,
-		scrollOffset: 180
+		fixedMargin: 140,
+		scrollOffset: marginTop
 	});
-
 })
